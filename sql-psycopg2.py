ee8c4ea5ec05bae1bf01d09df35149e4a95abbdc
Tripta Grey <tripta.grey@gmail.com>
2024-12-15 17:00:06 +0000

    install ps and add pscopg2 to run queries to Chinook db

---
+++ sql-psycopg2.py
@@ -0,0 +1,44 @@
+import psycopg2
+
+# connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# Query 1 - select all records from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - select only the "Name" records from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only by ArtistId #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only albums with the "Artist" #51 on the "Artist" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# Challenge Query 1- select all tracks where the composer is "AC/DC" from the "Track" table
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["AC/DC"])
+
+# Challenge Query 2- select all tracks where the composer is "test" from the "Track" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["test"])
+
+# fetch the results (multiple)
+results = cursor.fetchall()
+
+# fetch the results (single)
+# results = cursor.fetchone()
+
+# close the connection
+connection.close()
+
+# print results
+for result in results:
+    print(result)
